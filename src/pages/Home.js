4bd2f0398e19f7f46adc0e0cfa3250a7e9e397b8
kirnatia <natiakiria@gmail.com>
2023-09-14 20:29:33 -0400

    add home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { ProductContext } from '../contexts/ProductContext';
+import Product from '../components/Product';
 
 const Home = () => {
-  return <div>Homepage</div>;
+  const { products } = useContext(ProductContext);
+
+  const filteredProducts = products.filter(item => {
+    return item.category === "men's clothing" || item.category === "women's clothing"
+  });
+
+  return (
+    <div>
+      <section className='py-16'>
+        <div className="container mx-auto">
+          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-[20px] max-w-sm mx-auto md:max-w-none md:mx-0'>
+            {filteredProducts.map((product) => (
+              <Product key={product.id} product={product} />
+            ))}
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Home;
