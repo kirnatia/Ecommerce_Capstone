42608646313f6c22f269260ddc37b06e9453f983
kirnatia <natiakiria@gmail.com>
2023-09-19 00:09:08 -0400

    modified code for a single product details page

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Home from './pages/Home';
 import ProductDetails from './pages/ProductDetails';
-import SignupPage from './pages/SignUp';
-import LoginPage from './pages/Login';
+// import SignupPage from './pages/SignUp';
+// import LoginPage from './pages/Login';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 import Footer from './components/Footer'; 
@@ -17,8 +17,8 @@ const App = () => {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/product/:id' element={<ProductDetails />} />
-          <Route path="/" element={<LoginPage/>} />
-            <Route path="/" element={<SignupPage/>} />
+          {/* <Route path="/" element={<LoginPage/>} /> */}
+            {/* <Route path="/" element={<SignupPage/>} /> */}
         </Routes>
         <Sidebar />
         <Footer />
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return <footer className='bg-primary py-12'>
     <div className='container mx-auto'>
-      <p className='text-white text-center'>Copyright &copy; Capstone Project Fullstack Academy 2023</p>
+      <p className='text-white text-center'>Copyright &copy; Capstone Project | FA | Natia Kiria 2023</p>
     </div>
   </footer>;
 };
